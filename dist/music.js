19f3c4f9e98dacd2ec428e42fa9c80e893e9b9a6
YSC168 <1643671159@qq.com>
2019-06-30 01:08:01 +0800

    Site updated: 2019-06-30 01:07:41

--- dist/music.js
+++ dist/music.js
@@ -7,7 +7,31 @@ const ap = new APlayer({
         name: 'Sakura Tears',
         artist: '櫻花淚',
         url: 'http://music.163.com/song/media/outer/url?id=509313150.mp3',
-        cover: 'https://cdn.jsdelivr.net/gh/ysc168/eee@1.0/head.jpg',
+        cover: 'http://p1.music.126.net/HS9QYUXD3UCpmBxnMfxicA==/109951163604923548.jpg?param=180y180',
+      },
+      {
+        name: "Shape of You",
+        artist: 'J.Fla',
+        url: 'http://music.163.com/song/media/outer/url?id=468882985.mp3',
+        cover: 'http://p1.music.126.net/hZ2ttGYOQbL9ei9yABpejQ==/109951163032775841.jpg?param=130y130',
+      },
+     {
+        name: "你是人间四月天",
+        artist: '解忧邵帅',
+        url: 'http://music.163.com/song/media/outer/url?id=1344897943.mp3',
+        cover: 'http://p2.music.126.net/o9SkgZVnB2g0JDeeK-ugGA==/109951163886781910.jpg?param=130y130',
+      },
+        {
+        name: "我喜欢你",
+        artist: '小晏晏',
+        url: 'http://music.163.com/song/media/outer/url?id=520619606.mp3',
+        cover: 'http://p2.music.126.net/IzI61mZWGe0qfmL9RFzjjw==/109951163069340813.jpg?param=130y130',
+      },
+       {
+        name: "起风了",
+        artist: '辣椒券',
+        url: 'http://music.163.com/song/media/outer/url?id=1330348068.mp3',
+        cover: 'http://p2.music.126.net/diGAyEmpymX8G7JcnElncQ==/109951163699673355.jpg?param=130y130',
       },
        {
         name: "風になる",
@@ -21,30 +45,12 @@ const ap = new APlayer({
         url: 'http://music.163.com/song/media/outer/url?id=537854958.mp3',
         cover: 'http://p1.music.126.net/RWIn_QVvCl4GMa8qwCHySA==/109951163424252100.jpg?param=130y130',
       },
-      {
-        name: "起风了",
-        artist: '辣椒券',
-        url: 'http://music.163.com/song/media/outer/url?id=1330348068.mp3',
-        cover: 'http://p2.music.126.net/diGAyEmpymX8G7JcnElncQ==/109951163699673355.jpg?param=130y130',
-      },
         {
         name: "往后余生",
         artist: '王贰浪',
         url: 'http://music.163.com/song/media/outer/url?id=571338279.mp3',
         cover: 'http://p1.music.126.net/hTiVEeQTUSsc-YGF3o6ItQ==/109951163337847600.jpg?param=130y130',
       },
-      {
-        name: "我喜欢你",
-        artist: '小晏晏',
-        url: 'http://music.163.com/song/media/outer/url?id=520619606.mp3',
-        cover: 'http://p2.music.126.net/IzI61mZWGe0qfmL9RFzjjw==/109951163069340813.jpg?param=130y130',
-      },
-      {
-        name: "Shape of You",
-        artist: 'Ed Sheeran',
-        url: 'http://music.163.com/song/media/outer/url?id=451703096.mp3',
-        cover: 'http://p1.music.126.net/5Zs51JS6cQzvQclW5u_J1g==/18832435162240436.jpg?param=130y130',
-      },
-    
+   
     ]
 });
