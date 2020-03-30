d49cfa7aa92c85fda17ece71c5f33ca0b76a81d5
ysc1688 <1643671159@qq.com>
2020-03-30 11:32:51 +0800

    Site updated: 2020-03-30 11:32:49

--- dist/music.js
+++ dist/music.js
@@ -15,7 +15,6 @@ const ap = new APlayer({
         name: '世界这么大还是遇见你',
         artist: '程响',
         url: 'http://183.252.54.25/amobile.music.tc.qq.com/C400001xLIXo2w9V7U.m4a?guid=517866484&vkey=FE3B6C16AAD121F5CBCF400AC51DF9A864655AA64DDE8C4194628BC45E8EC1615FF724FA90FEF3F5CBFAC12E246FF9DD68BA71E8B179DEE1&uin=0&fromtag=66',
-        lrc: 'https://cdn.jsdelivr.net/gh/YSC168/cdn@2.1.5/mp3/shijienamoda.lrc',
         cover: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000003Ls5Jo4EFBIH_2.jpg?max_age=2592000',
       },
 
